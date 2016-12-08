57946c25fcd33a161696b99b0619595e5af66aa3
MikeCopp13 <copp1mc@cmich.edu>
2016-12-08 15:05:09 -0500

    Add files via upload

---
+++ js/base.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('#lets_ride_button').click(function(){
+        $('#lets_ride_lyrics').toggle()
+    })
+    $('#mama_siah_button').click(function(){
+        $('#mama_siah_lyrics').toggle()
+    })
+
+});
+$(document).ready(function () {
+    $(".fancybox").fancybox();
+});
+
+//syntax is $("selector").call();
+//http://www.w3schools.com/jquery/jquery_hide_show.asp
+//http://stackoverflow.com/questions/6718484/how-to-add-property-to-existing-class
+// Fancy Box 
+//http://fancyapps.com/fancybox/
+//
+//alt caption
+//http://jsfiddle.net/2LXNh/
